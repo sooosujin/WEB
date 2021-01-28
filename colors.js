dbc6a9b4a50251fd0fc05ce089bb5a693eabf40d
sooosujin <71065377+sooosujin@users.noreply.github.com>
2021-01-28 15:18:07 +0900

    js practice_6

---
+++ colors.js
@@ -0,0 +1,42 @@
+  var Links = {
+  setColor: function (color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length){
+      alist[i].style.color = color;
+
+      i = i +1;
+    }
+  // $('a').css('color', color);
+  }
+}
+
+var Body = {
+  setColor:function(color){
+  document.querySelector('body').style.color = color;
+    // $('body').css('color', color);
+  },
+  SetBackgroundColor:function (color){
+   document.querySelector('body').style.backgroundColor = color;
+    // $('a').css('backgroundColor', color);
+  }
+}
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night'){
+    Body.SetBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+
+  }  else {
+    Body.SetBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+  Links.setColor('skyblue');
+
+  }
+}
